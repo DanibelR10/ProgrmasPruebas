ce6b69d01c9c6d480f86289dfdc9571848d13b01
DanibelR10 <danibelruizmontero2003@gmail.com>
2025-05-02 23:14:29 -0400

    Add files via upload
    Programas del modulo 1

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite el primer numero: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Digite el segundo numero: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            if (num2 != 0)
+            {
+                double resultado = num1 / num2;
+                Console.WriteLine("Total de la division: " + resultado);
+            }
+            else
+            {
+                Console.WriteLine("Error: No se puede dividir entre cero.");
+            }
+        }
+    }
+}
